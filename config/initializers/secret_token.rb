c57faf9e4ad9517886a42a81a86bbf35232ef280
Marc Pérez <marcperez2000@gmail.com>
2012-03-19 13:16:48 +0100

    First import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wictasks::Application.config.secret_token = '5f0f63ffa63be9830d0450bcf5981029ee9bcf08a53f6ebecab33684a88eea50d4f2cda89e5b12c21e0804447d835e179a73a065c0d9fcfb06b4982b23383dfd'
