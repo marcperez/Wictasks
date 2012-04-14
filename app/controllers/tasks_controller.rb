c3912bb6992ff03f4f5e12e245574c0869a07f19
Marc Pérez <marcperez2000@gmail.com>
2012-04-14 22:30:25 +0200

    Added user-tasks relationship

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
 
     respond_to do |format|
       format.html # index.html.erb
@@ -42,7 +42,7 @@ class TasksController < ApplicationController
   # POST /tasks
   # POST /tasks.json
   def create
-    @task = Task.new(params[:task])
+    @task = current_user.tasks.new(params[:task])
 
     respond_to do |format|
       if @task.save
@@ -58,7 +58,7 @@ class TasksController < ApplicationController
   # PUT /tasks/1
   # PUT /tasks/1.json
   def update
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
 
     respond_to do |format|
       if @task.update_attributes(params[:task])
